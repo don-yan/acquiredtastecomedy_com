1925bbafd23f2ad8f3ea0c7cec6a365ff3b2f92d
Yan Kisen <yan.kisen@coresecure.com>
2024-03-25 23:00:23 -0400

    sample commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>hello</h1>
-    <NavBar />
-    <NuxtPage />
+  <h1 class="red-title">hello</h1>
+  <NavBar/>
+  <NuxtPage/>
 </template>
+<style scoped lang="css">
+h1.red-title {
+  color: red;
+}
+
+</style>
